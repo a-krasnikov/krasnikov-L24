fbd03e822c549f2959727369297dcc870d84d870
a-krasnikov <andriikrasnikov@gmail.com>
2021-03-12 14:35:38 +0200

    Test CreatePostUseCase

---
+++ app/src/test/java/krasnikov/project/postsapp/post/create/domain/CreatePostUseCaseTest.kt
@@ -0,0 +1,65 @@
+package krasnikov.project.postsapp.post.create.domain
+
+import io.mockk.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import krasnikov.project.postsapp.R
+import krasnikov.project.postsapp.post.common.data.PostRepository
+import krasnikov.project.postsapp.post.common.data.model.PostEntity
+import krasnikov.project.postsapp.post.create.domain.validate.PostValidator
+import krasnikov.project.postsapp.post.create.domain.validate.error.ValidationError
+import org.junit.After
+import org.junit.Before
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+
+@ExperimentalCoroutinesApi
+internal class CreatePostUseCaseTest {
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `if validation passed then post is created`() {
+        val repository = mockk<PostRepository>(relaxUnitFun = true)
+        val postValidator = mockk<PostValidator>(relaxUnitFun = true)
+        val post = mockk<PostEntity>()
+
+        val createPostUseCase = CreatePostUseCase(repository, postValidator, testDispatcher)
+        runBlockingTest { createPostUseCase.invoke(post) }
+
+        verifySequence {
+            postValidator.validate(post)
+            runBlockingTest { repository.savePost(post) }
+        }
+    }
+
+    @Test
+    fun `if validation fail then post is not created`() {
+        val repository = mockk<PostRepository>(relaxUnitFun = true)
+        val postValidator = mockk<PostValidator>() {
+            every { validate(any()) } throws ValidationError(R.string.error_post_body_length_validation)
+        }
+        val post = mockk<PostEntity>()
+
+        val createPostUseCase = CreatePostUseCase(repository, postValidator, testDispatcher)
+
+        assertFailsWith<ValidationError> { runBlockingTest { createPostUseCase.invoke(post) } }
+
+        verify { postValidator.validate(post) }
+        verify(inverse = true) { runBlockingTest { repository.savePost(post) } }
+    }
+}
